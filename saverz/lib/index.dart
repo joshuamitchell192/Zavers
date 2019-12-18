7cc6310891da0259f9ba21ed69d0682d771fff9d
Farah Ardina <farahbahudin@Farahs-MacBook-Pro.local>
2019-12-18 14:14:58 +1000

    Added Navigation to Pages and Refactored
    Added Navigation to Widgets in Pages Folder and refactored the home page to be separated into an index page with bottom navigation bar and the homePage in a separate widget within the pages folder

---
+++ saverz/lib/index.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:saverz/pages/home.dart';
+import 'package:saverz/pages/placeholder.dart';
+
+class Index extends StatefulWidget {
+  Index({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _IndexState createState() => _IndexState();
+}
+
+class _IndexState extends State<Index> {
+  int _counter = 0;
+  int _selectedIndex = 0;
+  final List<Widget> _pages = [
+    HomePage(),
+    PlaceholderWidget(Colors.amber[800], "Transactions"),
+    PlaceholderWidget(Colors.deepPurple, "Savings"),
+  ]; 
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  void onBottomNavBarTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      HapticFeedback.vibrate();
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+
+    Color iconColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+
+      body: Container(
+        alignment: Alignment.center,
+        child: _pages[_selectedIndex],
+      ),
+      
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      
+      bottomNavigationBar: BottomNavigationBar(
+
+        currentIndex: _selectedIndex,
+        onTap: onBottomNavBarTapped,
+
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.compare_arrows),
+            title: new Text('Transactions'),
+          ),
+
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.attach_money),
+            title: new Text('Savings')
+          ),
+        ],
+      ),
+
+    );
+  }
+}
---
+++ saverz/lib/pages/placeholder.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PlaceholderWidget extends StatelessWidget {
+ final Color color;
+ final String title;
+
+ PlaceholderWidget(this.color, this.title);
+
+ @override
+ Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: color,
+    body: Center(
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 24,
+          color: Colors.white,
+        ),
+      ),
+    )
+  );
+ }
+}
